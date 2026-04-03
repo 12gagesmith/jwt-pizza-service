56b8fae4239342f6b9796958548e36e975d5934b
Gage Smith <gagesmith242@gmail.com>
2026-04-03 16:41:59 -0600

    Added more to logger class, including httpLogger

--- src/service.js
+++ src/service.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/userRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics');
+const logger = require('./logger');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 app.use(metrics.requestTracker);
 app.use(metrics.activeUserTracker);
 app.use(metrics.latencyTracker);
+app.use(logger.httpLogger);
 
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
