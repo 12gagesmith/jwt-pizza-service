0094ae6365ab95bbdb52b84fb85a56cebd1d64f2
Gage Smith <gagesmith242@gmail.com>
2026-04-04 09:38:16 -0600

    Logs now sanitize token

--- src/logger.js
+++ src/logger.js
@@ -40,7 +40,8 @@ class Logger {
   
     sanitize(logData) {
       logData = JSON.stringify(logData);
-      return logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+      logData = logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+      return logData.replace(/\\*"token\\*":\s*\\*"[^"]*\\*"/g, '\\"token\\": \\"*****\\"');
     }
   
     sendLogToGrafana(event) {
